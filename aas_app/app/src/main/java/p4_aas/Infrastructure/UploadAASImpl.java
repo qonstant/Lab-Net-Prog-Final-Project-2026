a9516fafbedff01cdfbcd55379ac298ca120adb8
kali <kali@kali.kali>
2026-05-11 05:31:12 -0400

    fixed bugs

--- aas_app/app/src/main/java/p4_aas/Infrastructure/UploadAASImpl.java
+++ aas_app/app/src/main/java/p4_aas/Infrastructure/UploadAASImpl.java
@@ -1,18 +1,6 @@
-// Copyright 2023 riccardo.bacca@studio.unibo.it
-// 
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-// 
-//     http://www.apache.org/licenses/LICENSE-2.0
-// 
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package p4_aas.Infrastructure;
+
+import p4_aas.StaticProperties;
 
 import org.eclipse.basyx.aas.metamodel.map.AssetAdministrationShell;
 import org.eclipse.basyx.aas.metamodel.map.descriptor.AASDescriptor;
@@ -44,6 +32,6 @@ public class UploadAASImpl implements UploadAAS {
     }
 
     private String generateAASIP(Integer port) {
-        return String.format("http://localhost:%d/aas", port);
+        return String.format("http://%s:%d/aas", StaticProperties.AAS_PUBLIC_HOST, port);
     }
 }
--- aas_app/app/src/main/java/p4_aas/StaticProperties.java
+++ aas_app/app/src/main/java/p4_aas/StaticProperties.java
@@ -1,17 +1,3 @@
-// Copyright 2023 riccardo.bacca@studio.unibo.it
-// 
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-// 
-//     http://www.apache.org/licenses/LICENSE-2.0
-// 
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package p4_aas;
 
 /**
@@ -20,6 +6,7 @@ package p4_aas;
 public class StaticProperties {
     public static final String REGISTRY_POLLING_IP = "http://100.0.2.1:4000/registry/api/v1/registry";
     public static final String REGISTRYPATH = "http://100.0.2.1:4000/registry/";
+    public static final String AAS_PUBLIC_HOST = System.getenv().getOrDefault("AAS_PUBLIC_HOST", "10.0.2.15");
 
     public static final String SW1_MANAGEMENT_IP = "100.0.1.11";
     public static final String SW2_MANAGEMENT_IP = "100.0.1.12";
